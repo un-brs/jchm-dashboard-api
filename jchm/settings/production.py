1566a398bba5e137f967dab84954feafcf8b5515
Alexander Mazurov <alexander.mazurov@gmail.com>
2014-11-05 23:50:05 +0100

    Add jsonp renderer

--- jchm/settings/production.py
+++ jchm/settings/production.py
@@ -158,5 +158,10 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.JSONRenderer',        
+        'rest_framework.renderers.JSONPRenderer'
+    )
 }
